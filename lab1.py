6be3e526dfbef0b2b587f2af168ae6b4f2bf5518
nf561384-debug <nf561384@gmail.com>
2026-09-13 21:42:24 +0500

    Add files via upload

---
+++ lab1.py
@@ -0,0 +1,85 @@
+print ("Hello,World !")
+x=1
+#initial value of x is 1
+if x>0:
+    print("these are two comments") #print a string
+    txt=input("enter a number:")
+    print(txt)
+    print("Alishba");print("Nasir")
+
+# --------SIGNLE SPACE INDENDATION--------
+a=1
+if a>0:
+ print("This statement has single space indendation")
+ print("This statement has single space indendation")
+
+# --------SIGNLE TAB INDENDATION--------
+a=1
+if a>0:
+    print("This statement has single tab indendation")
+    print("This statement has single tab indendation")
+
+
+# --------SIGNLE SPACE+TAB INDENDATION--------
+a=1
+if a>0:
+     print("This statement has single space+tab indendation")
+     print("This statement has single space+tab indendation")
+
+# ---------DATA TYPES AND TYPE CASTING-------
+y=12
+print(type(y))
+i=(-2345)
+print(type(i))
+z=0.34
+print(type(z))
+d=.45
+print(type(d))
+f=(-9.78)
+print(type(f))
+j=2.13e-56
+print(type(j))
+k=5E5670
+print(type(k))
+l=complex(1,2)
+print(type(l))
+m=1+2j
+print(type(m))
+n=1+2J
+print(type(n))
+v=False
+print(type(v))
+#---STRINGS
+str="this is string"
+print(str)
+str1="day's"
+print(str1)
+print("This is backslash(\\) mark")
+print("This is tab \t key")
+print("These are \'single quotes\ '")
+print("These are \"double quotes\"")
+print("This is a newline \nNew line")
+#------indexing-----
+str3="PYTHON TUTORIAL"
+print(str[1])
+print(str[-12])
+
+#----list------
+list=[1,2,4,6]
+print(list)
+list2=["red","blue","green"]
+print(list2)
+list3=[1,4,5,"apple","Banana"]
+print(list3)
+list4=[]
+print(list)
+#----slicing---
+list5=["alishba","aleena","Hadiya"]
+print(list5[0],list[2])
+
+my_list=["Red","Blue","Green","Black"]
+print(my_list[0:2])
+
+print(my_list[1:-2])
+
+
